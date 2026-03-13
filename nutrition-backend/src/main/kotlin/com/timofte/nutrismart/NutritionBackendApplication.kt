e37f69e58e26b3594c0fae95fde4ac3037d5649d
ByteCoderNinja <alextimofte03@gmail.com>
2026-03-13 03:22:01 +0200

    Remediere eroare aplicatie

--- nutrition-backend/src/main/kotlin/com/timofte/nutrismart/NutritionBackendApplication.kt
+++ nutrition-backend/src/main/kotlin/com/timofte/nutrismart/NutritionBackendApplication.kt
@@ -2,12 +2,14 @@ package com.timofte.nutrismart
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
+@EnableJpaAuditing
 class NutritionBackendApplication
 
 fun main(args: Array<String>) {
